322ef44ebd25a2a5ead8fbb30fe58203a0aa60f9
schalk <sv@easybooking.at>
2024-01-06 07:18:42 +0100

    episode 8 start

--- handler.go
+++ handler.go
@@ -79,3 +79,45 @@ func handleToggleTask(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "CompletedCount", map[string]any{"Count": completed, "SwapOOB": true})
 
 }
+
+func handleDeleteTask(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("error parsing id into int %v", err)
+		return
+	}
+	err = deleteTask(r.Context(), id)
+	if err != nil {
+		log.Printf("error deleting task %v", err)
+		return
+	}
+
+	count, err := fetchCount()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	completed, err := fetchCompletedCount()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.ExecuteTemplate(w, "TotalCount", map[string]any{"Count": count, "SwapOOB": true})
+	tmpl.ExecuteTemplate(w, "CompletedCount", map[string]any{"Count": completed, "SwapOOB": true})
+}
+
+func handleEditTask(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("error parsing id into int %v", err)
+		return
+	}
+	item, err := fetchTask(id)
+	if err != nil {
+		log.Printf("error fetching task %v", err)
+		return
+	}
+	tmpl.ExecuteTemplate(w, "Item", map[string]any{"Item": item, "SwapOOB": true, "Editing": true})
+}
